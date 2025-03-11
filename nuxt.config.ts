c860755f0bdfe9d24104c5f1eded1a1917312799
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-03-11 20:43:13 +0900

    headlineをanchorLinkにしない

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,17 @@ export default defineNuxtConfig({
                 { rel: 'icon', type: 'image/png', href: '/favicon.png' },
             ]
         }
+    },
+    content: {
+        renderer: {
+            anchorLinks: {
+                h1: false,
+                h2: false,
+                h3: false,
+                h4: false,
+                h5: false,
+                h6: false,
+            }
+        }
     }
 })
