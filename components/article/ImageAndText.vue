486fd9f76e0acc8a120cb95a38588f1f2b7ab211
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-03-12 08:09:30 +0900

    テキストと画像を横並びにする部品追加

---
+++ components/article/ImageAndText.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const { imgPath, alt } = defineProps<{
+  imgPath: string;
+  alt: string;
+}>();
+</script>
+
+<template>
+  <div class="row">
+    <div class="content">
+      <slot />
+    </div>
+    <div class="image">
+      <figure>
+        <img :src="imgPath" :alt="alt" />
+      </figure>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  display: flex;
+  gap: 20px;
+}
+
+.image {
+  width: calc(55% - 10px);
+}
+
+.content {
+  width: calc(45% - 10px);
+}
+
+figure img {
+  width: 100%;
+}
+</style>
