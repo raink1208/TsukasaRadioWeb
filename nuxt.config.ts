3d01e93a5d3c1b75a461ee905e619809f361dc91
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-04-07 01:13:12 +0900

    アイコンをクライアントにバンドル

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,11 @@ export default defineNuxtConfig({
     icon: {
         serverBundle: {
             collections: ['mdi', 'prime', 'material-symbols', 'icon-park-outline']
+        },
+        clientBundle: {
+            icons: ['mdi', 'prime', 'material-symbols', 'icon-park-outline'],
+            scan: true,
+            sizeLimitKb: 256
         }
     }
 })
