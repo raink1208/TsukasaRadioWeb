2fef39abce8cb897f0e5568d37058d028df12964
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-04-07 07:58:49 +0900

    Clarity 読み込みタイミング変更

--- plugins/clarity.client.ts
+++ plugins/clarity.client.ts
@@ -1,5 +1,7 @@
 import Clarity from "@microsoft/clarity";
 
 export default defineNuxtPlugin(() => {
-    Clarity.init('qxwnr97w9j');
+    window.addEventListener('DOMContentLoaded', () => {
+        Clarity.init('qxwnr97w9j');
+    })
 });
