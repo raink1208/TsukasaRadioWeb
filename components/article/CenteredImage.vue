dca20252934fa98dd764b035106490fbc2d19f4b
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-04-18 08:22:38 +0900

    画像を中央に置くコンポーネント追加

---
+++ components/article/CenteredImage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { imgPath, alt, width } = defineProps<{
+  imgPath: string;
+  alt: string;
+  width: number;
+}>();
+
+</script>
+
+<template>
+  <div class="container">
+    <img :src="imgPath" :alt="alt" :style="{width: width}">
+    <caption>{{ alt }}</caption>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
