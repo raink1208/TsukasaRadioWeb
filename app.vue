8763758b0f25bc7d420da130a445f4f21ce4a74a
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-04-24 08:07:15 +0900

    更新

--- app.vue
+++ app.vue
@@ -26,3 +26,50 @@ provide("pauseBGM", pause);
   </div>
   <AudioPlayer ref="audioPlayer" />
 </template>
+
+<style>
+:root {
+  --base-color: #1f1f1f;
+  --base-sub-color: #1c1c1c;
+  --accent-color: #B92E46;
+  --sub-accent-color: #b78575;
+  --text-color: #d3d3d3;
+  --divider-color: rgba(255, 255, 255, 0.4);
+  --headline-font: "BIZ UDPGothic", sans-serif;
+  --text-font: "Meiryo UI", sans-serif;
+  --article-font: "Meiryo", sans-serif;
+  --bold-text-font: "BIZ UDPGothic", sans-serif;
+}
+
+html {
+  background-color: var(--base-color);
+  font-size: 18px;
+}
+
+html, p {
+  color: var(--text-color);
+}
+
+h2, h3 {
+  font-family: var(--headline-font);
+}
+
+p {
+  font-family: var(--text-font);
+}
+
+a {
+  text-decoration: none;
+}
+
+::-webkit-scrollbar{
+  width: 8px;
+}
+::-webkit-scrollbar-track{
+  background-color: var(--base-sub-color);
+}
+::-webkit-scrollbar-thumb{
+  background-color: var(--accent-color);
+  border-radius: 6px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -12,40 +12,6 @@
 </template>
 
 <style>
-:root {
-  --base-color: #1f1f1f;
-  --base-sub-color: #1c1c1c;
-  --accent-color: #B92E46;
-  --sub-accent-color: #b78575;
-  --text-color: #d3d3d3;
-  --divider-color: rgba(255, 255, 255, 0.4);
-  --headline-font: "BIZ UDPGothic", sans-serif;
-  --text-font: "Meiryo UI", sans-serif;
-  --article-font: "Meiryo", sans-serif;
-  --bold-text-font: "BIZ UDPGothic", sans-serif;
-}
-
-html {
-  background-color: var(--base-color);
-  font-size: 18px;
-}
-
-html, p {
-  color: var(--text-color);
-}
-
-h2, h3 {
-  font-family: var(--headline-font);
-}
-
-p {
-  font-family: var(--text-font);
-}
-
-a {
-  text-decoration: none;
-}
-
 .wrapper {
   display: flex;
   flex-direction: column;
@@ -60,6 +26,6 @@ a {
 
 footer {
   margin-top: auto;
-  width: 100%; /* 追加 */
+  width: 100%;
 }
 </style>
