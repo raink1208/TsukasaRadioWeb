f388d31c7660bffc1e1b584bf416054c7d429715
raink1208 <58714901+raink1208@users.noreply.github.com>
2025-08-09 20:47:58 +0900

    FirstView実装

--- components/FirstView.vue
+++ components/FirstView.vue
@@ -9,16 +9,28 @@ watch(() => props.isVisible, async (newValue) => {
   if (newValue) {
     await nextTick();
     const timeline = gsap.timeline();
-    timeline.from(".chara", {
-      x: '-35vw',
+
+    timeline.from(".desk", {
       opacity: 0,
-      duration: 1,
-      delay: 0.5,
-    }).from(".title", {
+      duration: 1
+    }).from(".character", {
+      x: '-15vw',
+      opacity: 0,
+      duration: 1.5
+    }).from(".onair", {
+      opacity: 0,
+      duration: 1
+    }).from(".white", {
+      opacity: 0,
+      duration: 1
+    }, "-=0.3").from(".title", {
       y: '80%',
       opacity: 0,
       duration: 1
-    });
+    }).from(".effect", {
+      opacity: 0,
+      duration: 1
+    }, "-=0.3");
     timeline.play();
   }
 });
@@ -26,7 +38,14 @@ watch(() => props.isVisible, async (newValue) => {
 
 <template>
   <div class="fv">
-    <img class="background" src="/img/background.png" alt="">
+    <div class="background-wrapper">
+      <img class="background" src="/img/firstview/background.webp" alt="bg">
+      <img class="white" src="/img/firstview/white.webp" alt="white">
+      <img class="character" src="/img/firstview/tsukasa.webp" alt="tsukasa">
+      <img class="desk" src="/img/firstview/desk.webp" alt="desk">
+      <img class="onair" src="/img/firstview/ONAIR.webp" alt="onair">
+      <img class="effect" src="/img/firstview/effect.webp" alt="effect">
+    </div>
     <h1 class="title">領国つかさの深夜通信-Dark Web-<br>非公式ファンサイト</h1>
   </div>
 </template>
@@ -40,20 +59,36 @@ watch(() => props.isVisible, async (newValue) => {
   position: relative;
 }
 
-.background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
+.background-wrapper {
+  position: relative;
   height: 100%;
-  object-fit: cover;
-  z-index: 0;
+  width: 100%;
+  overflow: hidden;
 }
 
+.background-wrapper img {
+  position: absolute;
+  bottom: -30px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.background-wrapper .character {
+  position: absolute;
+  bottom: -30px;
+  right: 0;
+
+  left: auto;
+  transform: none;
+}
+
+.background-wrapper .effect {
+  opacity: 0.8;
+}
 
 .title {
   position: absolute;
   top: 20%;
-  left: 10%;
+  left: 15%;
 }
 </style>
